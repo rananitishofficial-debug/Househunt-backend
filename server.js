30ae87be98671932ff82bc86f2fab25eb3d40f8c
rananitishofficial-debug <rananitish.official@gmail.com>
2025-11-16 20:03:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 connectDB();
-
+console.log("MONGO_URI VALUE=", process.env.MONGO_URI);
 app.use("/api/properties", propertyRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT ||5000;
 app.listen(PORT, () => console.log(`API running on ${PORT}`));
